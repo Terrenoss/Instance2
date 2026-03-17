d290d7518a935b709b07dc0ae74b36a9d3778c71
Terrenoss <thieryethan8@gmail.com>
2026-03-17 10:58:18 +0100

    Reapply "Merge branch 'dev' into feat-shader-dissolution"
    This reverts commit 73d6223414d00ac70b3de00503020ec785850c89.

---
+++ Assets/Scripts/Beatmap/LevelExporterEditor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(LevelExporter))]
+public class LevelExporterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LevelExporter exporter = (LevelExporter)target;
+
+        if (GUILayout.Button("Export Level"))
+        {
+            exporter.ExportLevel();
+        }
+    }
+}
+#endif
