456efab0bf90309d246941ec3e005018d90935a6
Terrenoss <thieryethan8@gmail.com>
2026-03-17 11:42:45 +0100

    Revert "Reapply "Merge branch 'dev' into feat-shader-dissolution""
    This reverts commit d290d7518a935b709b07dc0ae74b36a9d3778c71.

---
+++ Assets/Scripts/Sam/Sam_UI/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private SceneAsset level;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(level.name);
+        Cursor.visible = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
