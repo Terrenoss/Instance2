f06b78bcf0a5dca509b15e8534cd17c7db263693
PandorTaxe <beaumont.nathan0112@gmail.com>
2026-03-17 16:09:22 +0100

    Immunity time added to player

--- Assets/Scripts/Sam/TestSam_PlayerHealth/PlayerHealth.cs
+++ Assets/Scripts/Sam/TestSam_PlayerHealth/PlayerHealth.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -6,20 +7,39 @@ public class PlayerHealth : MonoBehaviour
     public int health = 3;
     
     [SerializeField] private ScoreManager scoreManager;
+    
+    [Header("Invincibility")]
+    [SerializeField] private float invincibilityDuration = 1.5f;
+    private bool isInvincible = false;
 
     public event Action OnPlayerHit;
     public event Action OnPlayerDeath;
 
     public void TakeDamage()
     {
+        if (isInvincible) return;
+        
         health--;
         scoreManager.DecreaseMultiplier();
         
         OnPlayerHit?.Invoke();
         
-        if (health <= 0 ) {
+        if (health <= 0 ) 
+        {
             Death();
+            return;
         }
+        
+        StartCoroutine(InvincibilityCoroutine());
+    }
+    
+    private IEnumerator InvincibilityCoroutine()
+    {
+        isInvincible = true;
+
+        yield return new WaitForSeconds(invincibilityDuration);
+
+        isInvincible = false;
     }
 
     public void Death()
