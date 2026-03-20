76e877dfd079f2c48ae97dc8bb221741c667d81a
xinom4966 <sd.dessay@gmail.com>
2026-03-20 17:24:50 +0100

    Preemptive fix because I'm very tired

--- Assets/Scripts/UI/CreditsBehaviour.cs
+++ Assets/Scripts/UI/CreditsBehaviour.cs
@@ -5,20 +5,25 @@ public class CreditsBehaviour : MonoBehaviour
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _duration = 5f;
     [SerializeField] private Transform _baseTransform;
+    [SerializeField] private GameObject _textContainer;
     private float _timer = 0f;
 
     private void OnEnable()
     {
         _timer = 0f;
-        transform.position = _baseTransform.position;
+        _textContainer.transform.position = _baseTransform.position;
     }
 
     private void Update()
     {
         if (_timer < _duration)
         {
-            transform.position += transform.up * _speed * Time.deltaTime;
+            _textContainer.transform.position += _textContainer.transform.up * _speed * Time.deltaTime;
             _timer += Time.deltaTime;
+        }
+        else
+        {
+            gameObject.SetActive(false);
         }
     }
 }
